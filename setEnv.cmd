4bab8d28f94ce61ba1de657d4f53de0cf5de77ad
Philipp Meier <phimei@gmx.ch>
2010-02-17 11:22:19 +0100

    README added

---
+++ setEnv.cmd
@@ -0,0 +1,9 @@
+@REM ======================================================================
+@REM === Sets environment vars for the current shell.
+@REM === Forces these vars to be in the PATH before centrally set vars.
+@REM ======================================================================
+
+
+set JAVA_HOME=C:\Java\jdk1.5.0_21
+
+set PATH=%JAVA_HOME%\bin;%PATH%
